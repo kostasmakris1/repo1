90b8586cb8956158373a9e1b26b3eae7c644f62c
Kostas Makris <kostas.makris@asos.com>
2018-05-23 14:35:28 +0100

    first commit

---
+++ myproject/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> myList = Arrays.asList(1,2,3);
+        List<Integer> myList1 = Arrays.asList(1,3,2);
+
+        System.out.println(myList.equals(myList1));
+
+
+
+    }
+}
