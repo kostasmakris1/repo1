e484d9f064ce2cd07ec34ef077c5d09e367016c1
Kostas Makris <kostas.makris@asos.com>
2018-05-25 13:10:59 +0100

    add tests

---
+++ assignment/src/test/java/pageobject/Locators.java
@@ -0,0 +1,11 @@
+package pageobject;
+
+public interface Locators {
+
+    public final static String SMART_DATA_WEB_APP_URL = "http://mosaic-test-app.s3-website.eu-west-2.amazonaws.com/#";
+    public final static String LOGIN_PAGE_SUBMIT_BTN = "input[type='submit']";
+    public final static String LOGIN_PAGE_USERNAME_FIELD = "input[placeholder='username']";
+    public final static String LOGIN_PAGE_PASSWORD_FIELD = "input[placeholder='password']";
+    public final static String SMART_PAGE_DASHBOARD_LABEL = "h1[class='h2']";
+    public final static String TABLE_DATA_BODY = "tbody";
+}
